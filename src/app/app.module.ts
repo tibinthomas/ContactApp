bbd255fa77fe1299429459633e491a2569119a2c
Tibin Thomas <tibinthomas@outlook.com>
2017-09-07 12:08:19 +0530

    1) Create 3 components.
    a) sidebar b) topbar c) mainwindow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MdSidenavModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainwindowComponent } from './mainwindow/mainwindow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    MainwindowComponent,
+    MdSidenavModule
   ],
   imports: [
     BrowserModule
