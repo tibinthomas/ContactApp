1ad355e47c54f67686c4783554156bb437177ecc
Tibin Thomas <tibinthomas@outlook.com>
2017-09-11 10:15:14 +0530

    Create Person model

---
+++ src/app/shared/person.model.ts
@@ -0,0 +1,15 @@
+export interface Person {
+         firstName: string;
+         lastName: string;
+         middleName?: string;
+         dateOfBirth: Date;
+         age?: number;
+         address: string;
+         profilePic: string;
+
+         phoneNo1: number;
+         phoneNo2: number;
+         email: string;
+         alternateAddress?: string;
+
+}
